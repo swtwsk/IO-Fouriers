ac1ed6f5fc03d5827286f593b1352d5ba0311d4d
przybyszewskiw <wp386044@students.mimuw.edu.pl>
2018-05-24 18:44:35 +0200

    Poprawiona i wstępnia przetestowana wersja klasy FileTranslator

--- app/src/main/java/pl/edu/mimuw/students/fouriersphone/FileTranslator.java
+++ app/src/main/java/pl/edu/mimuw/students/fouriersphone/FileTranslator.java
@@ -5,100 +5,179 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
+/**
+ * Klasa FileTranslator ma za zadanie zapewnić interfejs do komunikacji
+ * funkcji wywoływanych przez użytkownika z systemem plików. Jej podstawowe
+ * funkcjonalności to:
+ * getDownloadsFiles - wypisanie wszystkich plików o rozszerzeniu .txt z
+ * folderu Downloads;
+ * prepareFile - odczytanie danych z pliku to tablicy charów;
+ * saveFile - zapisanie Stringa do pliku.
+ * Należy pamiętać, że do poprawnego używania klasy użytkownik musi udzielić
+ * aplikacji zgody na zarządzanie pamięcią.
+ */
 public class FileTranslator {
 
-    /* Checks if external storage is available for read and write */
-    /* Code from https://developer.android.com/training/ */
-    public static boolean isExternalStorageWritable() {
+    /**
+     * Checks if external storage is available for read and write.
+     * Code adapted from https://developer.android.com/training/
+     * @return can we write to external storage
+     */
+    private static boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 
-    /* Checks if external storage is available to at least read */
-    /* Code from https://developer.android.com/training/ */
-    public static boolean isExternalStorageReadable() {
+    /**
+     * Checks if external storage is available to at least read.
+     * Code adapted from https://developer.android.com/training/
+     * @return can we read from external storage
+     */
+    private static boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
     }
 
-    /* Get directory for document */
-    public static File getPublicDocumentDir(String fileName) {
+    /**
+     * Function getting array of files from Downloads directory which
+     * are in .txt format.
+     * @return array of Strings - names of .txt format files in Downloads
+     */
+    public static String[] getDownloadsFiles() {
+        String formatEnding = ".txt";
+
+        File downloads = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
+
+        String[] files = downloads.list();
+        ArrayList<String> goodFiles = new ArrayList<>();
+
+        if (files == null) {
+            Log.d("File", "There are no files in Downloads directory");
+        } else {
+            for (String file : files) {
+                if (file.endsWith(formatEnding)) {
+                    goodFiles.add(file);
+                }
+            }
+        }
+        String[] goodFilesArray = goodFiles.toArray(new String[goodFiles.size()]);
+        Log.d("File", "It's a list of good files");
+        for (String file : goodFilesArray) {
+            Log.d("File", file);
+        }
+        return goodFilesArray;
+    }
+
+    /**
+     * Returns File object of a given file name in DIRECTORY_DOWNLOADS
+     * directory. If such file doesn't exist creates such file.
+     * @param fileName file name of wanted File object
+     * @return File object representing wanted file in Downloads or null
+     *         if unsuccessful
+     */
+    private static File getPublicDownloadDir(String fileName) {
         File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOCUMENTS), fileName);
+                Environment.DIRECTORY_DOWNLOADS), fileName);
         try {
             if (!file.createNewFile()) {
-                Log.e("File", "File exists");
+                Log.d("File", "File already existed");
             } else {
-                Log.e("File", "File created");
+                Log.d("File", "File created");
             }
         } catch (IOException e) {
-            Log.e("File", "Exception");
+            Log.e("File", "Exception in createNewFile");
             return null;
         }
         return file;
     }
 
-    /* Reads file into character array and return number of characters in file
-     * (-1 in case of error) */
+    /**
+     * When given a file name and array of chars, tries to find file of
+     * a given file name in Downloads and then reads its first bytes into
+     * given array of chars. It reads no more chars then size of file and
+     * no more then length of this array.
+     * @param fileName name of file in directory Downloads function should read
+     * @param cbuf array of chars function puts read chars into
+     * @return number of read chars or -1 in case of error
+     */
     public static int prepareFile(String fileName, char[] cbuf) {
         if (!isExternalStorageReadable()) {
-            Log.e("File", "Problems with reading");
+            Log.e("File", "Error - external storage is not readable");
             return -1;
         }
 
-        File file = getPublicDocumentDir(fileName);
-        FileReader reader = null;
+        File file = getPublicDownloadDir(fileName);
+        FileReader reader;
+
+        if (file == null) {
+            Log.e("File", "Problems with opening file");
+            return -1;
+        }
 
         try {
             reader = new FileReader(file);
         } catch (FileNotFoundException e) {
-            Log.e("File", "Exception");
+            Log.e("File", "Exception in creating FileReader");
             return -1;
         }
 
         try {
             return reader.read(cbuf);
         } catch (IOException e) {
-            Log.e("File", "Error in read");
+            Log.e("File", "Exception in read");
             return -1;
         }
     }
 
-    /* Saves array of characters to a file */
-    public static boolean saveFile(String filename, char[] cbuf) {
+    /**
+     * When given a file name of file in Downloads and a string it tries
+     * to write this string to this file. If file of given file name doesn't
+     * exist it creates it. On the other hand, if such file exists, it's
+     * overwritten.
+     * @param fileName name of file in Downloads function should write to
+     * @param s string function should write to file
+     * @return true if everything was successful and false if something went wrong
+     */
+    public static boolean saveFile(String fileName, String s) {
         if (!isExternalStorageWritable()) {
-            Log.e("File", "Can't write");
+            Log.e("File", "Error - external storage is not writable");
             return false;
         }
 
-        File file = getPublicDocumentDir(filename);
-        FileWriter writer = null;
+        File file = getPublicDownloadDir(fileName);
+        FileOutputStream stream = null;
+        boolean wasError = false;
 
         try {
-            writer = new FileWriter(file);
+            stream = new FileOutputStream(file);
+            stream.write(s.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e("File", "Exception in constructor of FileOutputStream");
+            wasError = true;
         } catch (IOException e) {
-            Log.e("File", "Exception");
-            return false;
+            e.printStackTrace();
+            Log.e("File", "Exception in function write");
+            wasError = true;
         }
 
-        try {
-            writer.write(cbuf);
-        } catch (IOException e) {
-            Log.e("File", "Error in write");
-            return false;
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("File", "Exception in function close");
+                wasError = true;
+            }
         }
 
-        return true;
+        return !wasError;
     }
 }
